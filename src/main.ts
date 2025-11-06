6a45784f510d97f1aa6b5042083afc9b145e2368
kelechiibeleche <kelechibeleche@gmail.com>
2025-11-06 18:02:09 +0100

    lab done

--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,28 @@
-// src/main.ts
+import {
+  getLocation,
+  getCurrentWeather,
+  displayLocation,
+  displayWeather,
+} from "./utils";
+
+const form = document.querySelector("form");
+const input = document.querySelector("input") as HTMLInputElement;
+
+form?.addEventListener("submit", async (event) => {
+  event.preventDefault();
+  const locationName = input.value.trim();
+  if (!locationName) return;
+
+  try {
+    const locationData = await getLocation(locationName);
+    const location = locationData.results
+      ? locationData.results[0]
+      : locationData[0];
+    displayLocation(location);
+
+    const weatherData = await getCurrentWeather(location);
+    displayWeather(weatherData);
+  } catch (error) {
+    console.error("Error fetching data:", error);
+  }
+});
--- src/utils.ts
+++ src/utils.ts
@@ -1,14 +1,38 @@
 // src/utils.ts
 
-import axios from 'axios';
-import { LocationResponse, Location } from "./types";
-
-
+import axios from "axios";
+import { LocationResponse, Location, WeatherResponse } from "./types";
 
 export function getLocation(locationName: string): Promise<LocationResponse> {
-    const url = `https://geocoding-api.open-meteo.com/v1/search?name=${locationName}&count=1`;
-    return axios.get(url).then((response) => response.data);
+  const url = `https://geocoding-api.open-meteo.com/v1/search?name=${locationName}&count=1`;
+  return axios.get(url).then((response) => response.data);
 }
 
+export function getCurrentWeather(
+  locationDetails: Location
+): Promise<WeatherResponse> {
+  return axios
+    .get(
+      `https://api.open-meteo.com/v1/forecast?latitude=${locationDetails.latitude}&longitude=${locationDetails.longitude}&current_weather=true&models=icon_global`
+    )
+    .then((response) => response.data);
+}
 
+export function displayLocation(locationDetails: Location): void {
+  const locationNameElement = document.getElementById("location-name");
+  const countryElement = document.getElementById("country");
 
+  if (locationNameElement)
+    locationNameElement.textContent = locationDetails.name;
+  if (countryElement) countryElement.textContent = locationDetails.country;
+}
+
+export function displayWeather(weatherData: WeatherResponse): void {
+  const temperatureElement = document.getElementById("temperature");
+  const windspeedElement = document.getElementById("windspeed");
+
+  if (temperatureElement)
+    temperatureElement.textContent = `${weatherData.current_weather.temperature}°C`;
+  if (windspeedElement)
+    windspeedElement.textContent = `${weatherData.current_weather.windspeed} km/h`;
+}
